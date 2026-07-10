a6265dbd16b678f89cbd35d98c692e02b34e4902
vishu601 <vishu601@gmail.com>
2026-07-10 07:53:10 +0000

    Fixed login redirect paths for Render

--- kiranakhata/settings.py
+++ kiranakhata/settings.py
@@ -116,3 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+
+LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'dashboard'
